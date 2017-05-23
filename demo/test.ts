8c61732f43e21117d7eb5e77fdc9a7b7122ba987
rtrompier <remy.trompier@gmail.com>
2017-05-23 16:34:34 +0200

    Add licence

--- demo/test.ts
+++ demo/test.ts
@@ -1,3 +1,11 @@
+/*
+ *  @license
+ *  Copyright Hôpitaux Universitaires de Genève. All Rights Reserved.
+ *
+ *  Use of this source code is governed by an Apache-2.0 license that can be
+ *  found in the LICENSE file at https://github.com/DSI-HUG/dejajs-components/blob/master/LICENSE
+ */
+
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import { getTestBed } from '@angular/core/testing';
